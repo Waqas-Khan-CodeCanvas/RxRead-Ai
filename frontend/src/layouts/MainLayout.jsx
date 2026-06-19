4fdbf5842021ed2f17879c07684007b7ce206342
waqas-khan-codecanvas <waqaskhan11411704@gmail.com>
2026-06-19 23:50:02 +0500

    UI/UX improved

--- frontend/src/layouts/MainLayout.jsx
+++ frontend/src/layouts/MainLayout.jsx
@@ -5,11 +5,20 @@ import Footer from '../components/Footer'
 
 const MainLayout = () => {
   return (
-    <div className="min-h-screen flex flex-col bg-slate-900">
+    <div className="relative min-h-screen flex flex-col bg-slate-950 overflow-x-hidden">
+      
+      {/* Global background */}
+      <div className="pointer-events-none fixed inset-0 -z-10">
+        <div className="absolute top-0 left-0 w-[500px] h-[500px] bg-cyan-500/8 blur-[140px] rounded-full" />
+        <div className="absolute bottom-0 right-0 w-[500px] h-[500px] bg-blue-600/8 blur-[140px] rounded-full" />
+      </div>
+
       <Navbar />
+
       <main className="flex-1">
         <Outlet />
       </main>
+
       <Footer />
     </div>
   )
